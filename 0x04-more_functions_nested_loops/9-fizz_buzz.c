92e4fba37ed3bbec93e7def7dc09500e9edc5381
MichaelSparks004 <chiazokaobasi03@gmail.com>
2022-07-08 09:58:59 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,38 +4,39 @@
 /**
  * main -> printing to 100
  * but for multiples of three, print fizz
- * and for the multiples of five, print buzzi
+ * and for the multiples of five, print buzz
  *
  * Return: Always 0 (Succesa)
  */
 
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
-}
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
-}
-else if (i == 1)
-{
-printf("%d", i);
-}
-else
-{
-printf(" %d", i);
-}
-}
-printf("\n")
-return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
